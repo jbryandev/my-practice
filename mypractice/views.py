c55e033dacb0ee0e432d4e5ec38ee4435d218efa
James Bryan <james.bryan.home@gmail.com>
2019-10-21 16:32:07 -0500

    Revert "Added project model fields"
    This reverts commit 5326754cb29cc72cea4431399d829b887862666e.

--- mypractice/views.py
+++ mypractice/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+@login_required
+def index(request):
+    return HttpResponse("<h1>My Practice</h1>")
--- projects/models.py
+++ projects/models.py
@@ -4,12 +4,8 @@ from django.db import models
 
 class Project(models.Model):
     project_name = models.CharField(max_length=200)
-    client_name = models.CharField(max_length=200)
     start_date = models.DateTimeField('start date')
     end_date = models.DateTimeField('end date')
     project_location = models.CharField(max_length=200)
-    project_number = models.CharField(max_length=10)
-    project_manager = models.CharField(max_length=200)
-    project_writeup = models.TextField()
     def __str__(self):
         return self.project_name
