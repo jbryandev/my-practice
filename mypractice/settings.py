61cf8fdce3ab8143317939ceb2ead58ffe27edc1
James Bryan <james.bryan.home@gmail.com>
2019-10-21 16:14:16 -0500

    Messing with pyscopg settings

--- mypractice/settings.py
+++ mypractice/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'mypractice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_pyscopg2',
+        'NAME': 'mypractice',
+        'USER': 'james_bryan',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '',
     }
 }
 
