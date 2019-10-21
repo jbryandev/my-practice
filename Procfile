e44d533723f4636812857279f0762604b556ca32
James Bryan <james.bryan.home@gmail.com>
2019-10-21 16:46:06 -0500

    Wipeout broken app

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn mypractice.wsgi --log-file -
--- mypractice/views.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-
-@login_required
-def index(request):
-    return HttpResponse("<h1>My Practice</h1>")
