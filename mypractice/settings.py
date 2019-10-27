1db812ba218001fecbdd8c5833b583a081d3d500
James Bryan <james.bryan.home@gmail.com>
2019-10-26 22:40:32 -0500

    Migrate DB to postgresql

--- mypractice/settings.py
+++ mypractice/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'mypractice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mypractice',
+        'USER': 'jamesbryan',
+        'PASSWORD': 'Warhawk83',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
