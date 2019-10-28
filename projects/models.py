d568d596c87a4913e87ee7fc8b0d19d3fd4c0834
James Bryan <james.bryan.home@gmail.com>
2019-10-27 19:44:30 -0500

    Added model fields to project

--- projects/models.py
+++ projects/models.py
@@ -7,5 +7,9 @@ class Project(models.Model):
     start_date = models.DateTimeField('start date')
     end_date = models.DateTimeField('end date')
     project_location = models.CharField(max_length=200)
+    client_name = models.CharField(max_length=200)
+    project_manager = models.CharField(max_length=200)
+    project_number = models.CharField(max_length=10)
+    project_writeup = models.TextField()
     def __str__(self):
         return self.project_name
--- projects/views.py
+++ projects/views.py
@@ -7,14 +7,12 @@ from .models import Project
 
 # Create your views here.
 
-#@login_required
 class IndexView(generic.ListView):
     template_name = 'projects/index.html'
 
     def get_queryset(self):
         return Project.objects.order_by('-start_date')
 
-#@login_required
 class DetailView(generic.DetailView):
     model = Project
     template_name = 'projects/detail.html'
