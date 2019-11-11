d7a8893b92f7f6a1809facc4331a5425601c89c9
James Bryan <james.bryan.home@gmail.com>
2019-11-11 10:59:36 -0600

    Automated pdf2text conversion of agendas

--- council/models.py
+++ council/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from datetime import datetime
 import requests
 import io
@@ -37,16 +38,27 @@ class Agenda(models.Model):
     agenda_date = models.DateField(null=False, blank=False)
     date_added = models.DateTimeField(null=True, blank=True)
     pdf_link = models.URLField(null=True, blank=True, max_length=500)
+    agenda_text = models.TextField(null=True, blank=True)
     department = models.ForeignKey(Department, related_name='agendas', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.agenda_date.strftime("%x")
 
+    def get_absolute_url(self):
+        return reverse('council:agenda-detail', args=[str(self.id)])
+
     def get_text(self):
-        text = ""
-        r = requests.get(self.pdf_link)
-        f = io.BytesIO(r.content)
-        images = convert_from_bytes(f.read())
-        for image in images:
-            text += str(((pytesseract.image_to_string(image)))) 
-        return text
+        if not self.agenda_text:
+            print("Agenda has not been converted to text yet. Attempting to conver from PDF...")
+            text = ""
+            r = requests.get(self.pdf_link)
+            f = io.BytesIO(r.content)
+            images = convert_from_bytes(f.read())
+            for image in images:
+                text += str(((pytesseract.image_to_string(image)))) 
+            self.agenda_text = text
+            self.save(update_fields=['agenda_text'])
+            return self.agenda_text
+        else:
+            print("Agenda has already been converted to text from PDF. Displaying resulting text.")
+            return self.agenda_text
