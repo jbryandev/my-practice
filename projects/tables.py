3a4f1cec3b8a0053f250849e71af84c07791af02
James Bryan <james.bryan.home@gmail.com>
2020-02-03 20:36:53 -0600

    Update council page styles

--- projects/tables.py
+++ projects/tables.py
@@ -9,5 +9,8 @@ class ProjectTable(tables.Table):
     class Meta:
         """ Meta class for Projects index table """
         model = Project
-        template_name = "django_tables2/bootstrap.html"
+        template_name = "django_tables2/bootstrap4.html"
         fields = ("project_name", "client_name", "project_location")
+        attrs = {
+            "class": "table table-striped"
+        }
