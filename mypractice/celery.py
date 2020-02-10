4b80614a0e60dc46064fa2bcd83a7c7e19e9fa42
James Bryan <james.bryan.home@gmail.com>
2020-02-10 17:16:38 -0600

    Test celery beat tasks

--- mypractice/celery.py
+++ mypractice/celery.py
@@ -15,18 +15,19 @@ app.autodiscover_tasks()
 # To run celery beat, use the following command:
 # celery -A mypractice beat -l info
 
+
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
 
     # Calls test('hello') every 10 seconds.
-    sender.add_periodic_task(10.0, test.s('hello'), name='add every 10')
+    #sender.add_periodic_task(10.0, test.s('hello'), name='add every 10')
 
     # Calls test('world') every 30 seconds
-    sender.add_periodic_task(30.0, test.s('world'), expires=10)
+    #sender.add_periodic_task(30.0, test.s('world'), expires=10)
 
     # Executes every Monday morning at 7:30 a.m.
     sender.add_periodic_task(
-        crontab(hour=7, minute=30, day_of_week=1),
+        crontab(hour=17, minute=20, day_of_week=1),
         test.s('Happy Mondays!'),
     )
 
