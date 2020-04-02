80d57e74c213e33943ba83416091b3ea2e3cb996
James Bryan <james.bryan.home@gmail.com>
2020-04-02 08:36:19 -0500

    Work on fetch agendas scheduler

---
+++ council/management/commands/fetch_agendas_nightly.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+from council.tasks import fetch_agendas
+from council.models import Department
+
+class Command(BaseCommand):
+    help = 'Runs fetch agendas command nightly to keep agendas up-to-date'
+
+    def handle(self, *args, **options):
+        self.stdout.write("---RUNNING FETCH AGENDAS NIGHTLY TASK---")
+        departments = Department.objects.all()
+        for department in departments:
+            self.stdout.write(f'Fetching agendas for {department.agency} - {department.department_name}')
+            #fetch_agendas.delay(department.id)
--- council/scheduler.py
+++
@@ -1,8 +0,0 @@
-from council.tasks import fetch_agendas
-from council.models import Department
-
-print("---RUNNING FETCH AGENDAS NIGHTLY TASK---")
-departments = Department.objects.all()
-for department in departments:
-        print("Fetching agendas for {} - {}".format(department.agency, department.department_name))
-        #fetch_agendas.delay(department.id)
