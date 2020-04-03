428fecba9fd8deff4248f23395bb02b6a7f4d113
James Bryan <james.bryan.home@gmail.com>
2020-04-03 13:58:30 -0500

    Change crawler import path to relative

--- council/crawlers/CrawlerFactory.py
+++ council/crawlers/CrawlerFactory.py
@@ -2,6 +2,6 @@ from importlib import import_module
 
 def create_crawler(department, progress_recorder):
     name = department.crawler.crawler_name
-    module = import_module(".crawlers.{}".format(name))
+    module = import_module(".crawlers.{}".format(name), package="council")
     crawler = getattr(module, name)(department, progress_recorder)
     return crawler
