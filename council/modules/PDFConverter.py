ba87a6d4e4ad820820a2fead48c114ee9c3ad2bf
James Bryan <james.bryan.home@gmail.com>
2020-04-06 15:45:16 -0500

    Refactor PDFConverter

--- council/modules/PDFConverter.py
+++ council/modules/PDFConverter.py
@@ -1,14 +1,13 @@
-import io
-import re
-import requests
-from pdf2image import convert_from_bytes
+import io, os, re, requests, tempfile
+from pdf2image import convert_from_path
 import pytesseract
 from .ImageProcessor import ImageProcessor
 
 class PDFConverter:
 
-    def __init__(self, pdf_url):
+    def __init__(self, pdf_url,):
         self.pdf_url = pdf_url
+        self.temp_dir = tempfile.TemporaryDirectory()
 
     def convert_pdf(self):
         request = self.request_pdf()
@@ -17,35 +16,34 @@ class PDFConverter:
         i = 1
         pdf_text = ""
         for image in images:
-            status = "Converting page {} of {}...".format(i, len(images))
-            print("PDF2Text: {}".format(status))
             processed_image = self.process_image(image)
             pdf_text += "{}".format(self.extract_text(processed_image))
-            print("PDF2Text: Conversion of page " + str(i) + " complete.")
             i += 1
             match = re.search("adjourn", pdf_text, re.IGNORECASE)
             if match:
-                print("PDF2Text: Adjourn keyword found. PDF2Text operation complete.")
                 break
         return pdf_text
 
     def request_pdf(self):
-        print("PDF2Text: Getting HTTP response...")
         return requests.get(self.pdf_url)
 
     def read_pdf(self, request):
-        print("PDF2Text: Downloading PDF file...")
         return io.BytesIO(request.content)
 
     def get_images(self, pdf_file):
-        print("PDF2Text: Converting PDF to images...")
-        return convert_from_bytes(pdf_file.read())
+        images = None
+        fh, temp_filename = tempfile.mkstemp()
+        with open(temp_filename, "wb") as f:
+            f.write(pdf_file.read())
+            f.flush()
+            images = convert_from_path(f.name)
+        os.close(fh)
+        os.remove(temp_filename)
+        return images
 
     def process_image(self, pdf_image):
-        print("PDF2Text: Pre-processing image...")
         processor = ImageProcessor(pdf_image)
         return processor.process()
 
     def extract_text(self, pdf_image):
-        print("PDF2Text: Extracting text using OCR...")
         return pytesseract.image_to_string(pdf_image)
