0dae332e349e969274bd30bf3aefe6f50aec1cdc
James Bryan <james.bryan.home@gmail.com>
2020-04-06 21:08:02 -0500

    Shorten celery task delay

--- council/modules/CouncilRecorder.py
+++ council/modules/CouncilRecorder.py
@@ -3,7 +3,7 @@ from celery_progress.backend import ProgressRecorder
 
 class CouncilRecorder(ProgressRecorder):
 
-    def update(self, start, end, message, delay=3):
+    def update(self, start, end, message, delay=1):
         print(message)
         self.set_progress(start, end, message)
         time.sleep(delay)
