bb63942502930a2286c33f5c949db683214a81be
James Bryan <james.bryan.home@gmail.com>
2020-04-20 09:07:10 -0500

    OCR refactoring

--- council/modules/OCRProcessor.py
+++ council/modules/OCRProcessor.py
@@ -7,9 +7,9 @@ class OCRProcessor:
     def process(self, image, config, extension):
         output_file = tempfile.NamedTemporaryFile(delete=False)
         input_file = tempfile.NamedTemporaryFile(delete=False)
-        image.save(input_file, "ppm")
-        print(['tesseract', input_file.name, output_file.name, config, extension])
-        process = subprocess.Popen(['tesseract', input_file.name, output_file.name, config, extension], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        input_file_name = input_file.name + os.extsep + "ppm"
+        image.save(input_file_name, "ppm")
+        process = subprocess.Popen(['tesseract', input_file_name, output_file.name, config, extension], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         process.communicate()
 
         with open(output_file.name, 'r') as handle:
