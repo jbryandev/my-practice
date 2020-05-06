f4fe25832fa768d0506228c23f5fc3b168e0087d
James Bryan <james.bryan.home@gmail.com>
2020-05-05 23:52:06 -0500

    NormanConverter

--- council/converters/NormanConverter.py
+++ council/converters/NormanConverter.py
@@ -12,19 +12,18 @@ class NormanConverter(PDFConverter):
         corrected_text = self.fix_ocr(trimmed_text)
         remove_attachments = self.remove_attachments(corrected_text)
         remove_actions = self.remove_actions(remove_attachments)
-        return remove_actions
-        # print(repr(remove_actions))
-        # self.indent_text(remove_actions)
-        # return self.formatted_text
+        print(repr(remove_actions))
+        self.indent_text(remove_actions)
+        return self.formatted_text
 
     def fix_ocr(self, trimmed_text):
-        fixed_text = trimmed_text.replace("\x0c", "")
+        fixed_text = trimmed_text.replace("\x0c", "\n")
         return fixed_text
  
     def indent_text(self, trimmed_text):
-        if re.match(r"(\d{1,2}\s)*[A-Z]+[a-z]*", trimmed_text):
-            start = re.match(r"(\d{1,2}\s)*[A-Z]+[a-z]*", trimmed_text)
-            end = re.search(r"\n(\d{1,2}\s)*[A-Z]+[a-z]*", trimmed_text[start.end():])
+        if re.match(r"\d{1,2}\s[A-Z][a-z]+|\d{1,2}\s([A-Z]|\d)+-\d{4}-\d+", trimmed_text):
+            start = re.match(r"\d{1,2}\s[A-Z][a-z]+|\d{1,2}\s([A-Z]|\d)+-\d{4}-\d+", trimmed_text)
+            end = re.search(r"\n\d{1,2}\s[A-Z][a-z]+|\n\d{1,2}\s([A-Z]|\d)+-\d{4}-\d+|\n\n", trimmed_text[start.end():])
             if end:
                 self.formatted_text += "<div class=\"mb-3\">{}</div>\n\n".format(
                     trimmed_text[start.start():end.start()+start.end()].strip().replace("\n", " ")
@@ -34,29 +33,17 @@ class NormanConverter(PDFConverter):
                 self.formatted_text += "<div class=\"mb-3\">{}</div>\n\n".format(
                     trimmed_text[start.start():].strip().replace("\n", " ")
                 )
-        # elif re.match(r"[A-Z]{3,}", trimmed_text):
-        #     start = re.match(r"[A-Z]{3,}", trimmed_text)
-        #     end = re.search(r"\n(\d{1,2}|[A-Z])\)\s[A-Z][A-Za-z]{2,}|\n[A-Z]{3,}", trimmed_text[start.end():])
-        #     if end:
-        #         self.formatted_text += "<div class=\"mb-3\">{}</div>\n\n".format(
-        #             trimmed_text[start.start():end.start()+start.end()].strip().replace("\n", " ")
-        #         )
-        #         self.indent_text(trimmed_text[end.start()+start.end():].strip())
-        #     else:
-        #         self.formatted_text += "<div class=\"mb-3\">{}</div>\n\n".format(
-        #             trimmed_text[start.start():].strip().replace("\n", " ")
-        #         )
-        # else:
-        #     end = re.search(r"\n(\d{1,2}\s)*[A-Z]+[a-z]*", trimmed_text)
-        #     if end:
-        #         self.formatted_text += "<div class=\"mb-3\" style=\"padding-left: 0.25in\">{}</div>\n\n".format(
-        #             trimmed_text[:end.start()].strip().replace("\n", " ")
-        #         )
-        #         self.indent_text(trimmed_text[end.start():].strip())
-        #     else:
-        #         self.formatted_text += "<div class=\"mb-3\" style=\"padding-left: 0.25in\">{}</div>\n\n".format(
-        #             trimmed_text.strip().replace("\n", " ")
-        #         )
+        else:
+            end = re.search(r"\n\d{1,2}\s[A-Z][a-z]+|\n\d{1,2}\s([A-Z]|\d)+-\d{4}-\d+|\n\n", trimmed_text)
+            if end:
+                self.formatted_text += "<div class=\"mb-3\" style=\"text-decoration: underline\">{}</div>\n\n".format(
+                    trimmed_text[:end.start()].strip().replace("\n", " ")
+                )
+                self.indent_text(trimmed_text[end.start():].strip())
+            else:
+                self.formatted_text += "<div class=\"mb-3\" style=\"text-decoration: underline\">{}</div>\n\n".format(
+                    trimmed_text.strip().replace("\n", " ")
+                )
 
     @staticmethod
     def crop_image(pdf_image):
@@ -66,25 +53,21 @@ class NormanConverter(PDFConverter):
         return pdf_image.crop(crop_boundaries)
 
     def remove_attachments(self, text):
-        replace_strings_list = []
-        replace_ranges_list = []
+        slice_ranges_list = []
         attachment_text = re.finditer(r"Attachments:", text)
         for attachment in attachment_text:
             action_text = re.search(r"ACTION NEEDED:", text[attachment.start():])
-            replace_strings_list.append(text[attachment.start():attachment.start()+action_text.start()])
-            replace_ranges_list.append({"start": attachment.start(), "end": attachment.start()+action_text.start()})
-        print(replace_ranges_list)
-        for text_range in replace_ranges_list:
-            print(type(text_range))
-            print(text_range)
-            print(text_range.get("end") - text_range.get("start"))
-        return re.sub("|".join(replace_strings_list), "", text)
+            slice_ranges_list.append((attachment.start(), attachment.start()+action_text.start()-1))
+        new_string = "".join(text[idx] for idx in range(len(text))\
+            if not any(front <= idx <= rear for front, rear in slice_ranges_list))
+        return new_string
 
     def remove_actions(self, text):
-        replace_strings_list = []
+        slice_ranges_list = []
         action_text = re.finditer(r"ACTION NEEDED:", text)
         for action in action_text:
             end_of_action = re.search(r"ACTION TAKEN:\n", text[action.start():])
-            replace_strings_list.append(text[action.start():action.start()+end_of_action.end()])
-        print(replace_strings_list)
-        return re.sub("|".join(replace_strings_list), "", text)
+            slice_ranges_list.append((action.start(), action.start()+end_of_action.end()-1))
+        new_string = "".join(text[idx] for idx in range(len(text))\
+            if not any(front <= idx <= rear for front, rear in slice_ranges_list))
+        return new_string
