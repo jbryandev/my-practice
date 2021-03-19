0dde57c91509afde46c2bcd56b1e243c2d42ea09
James Bryan <james.bryan.home@gmail.com>
2021-03-18 22:03:43 -0500

    No chrome options

--- council/modules/chromedriver.py
+++ council/modules/chromedriver.py
@@ -10,19 +10,19 @@ def set_chrome_options():
     This function sets the chrome options for the driver.
     """
     chrome_options = webdriver.ChromeOptions()
-    #chrome_options.binary_location = os.environ.get("CHROME_PATH")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--remote-debugging-port=9222")
-    chrome_options.add_argument("--allow-insecure-localhost")
-    chrome_options.add_argument("--ignore-certificate-errors")
-    chrome_options.add_argument("--ignore-ssl-errors")
-    chrome_options.add_argument("--allow-running-insecure-content")
-    chrome_options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36")
-    chrome_options.add_experimental_option("useAutomationExtension", False) # Avoids unpacked extensions error dialog box
-    chrome_options.add_experimental_option("excludeSwitches", ['enable-logging', 'enable-automation']) # Silences devtools listening alert
+    # chrome_options.binary_location = os.environ.get("CHROME_PATH")
+    # chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--disable-dev-shm-usage")
+    # chrome_options.add_argument("--no-sandbox")
+    # chrome_options.add_argument("--disable-gpu")
+    # chrome_options.add_argument("--remote-debugging-port=9222")
+    # chrome_options.add_argument("--allow-insecure-localhost")
+    # chrome_options.add_argument("--ignore-certificate-errors")
+    # chrome_options.add_argument("--ignore-ssl-errors")
+    # chrome_options.add_argument("--allow-running-insecure-content")
+    # chrome_options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36")
+    # chrome_options.add_experimental_option("useAutomationExtension", False) # Avoids unpacked extensions error dialog box
+    # chrome_options.add_experimental_option("excludeSwitches", ['enable-logging', 'enable-automation']) # Silences devtools listening alert
 
     return chrome_options
 
